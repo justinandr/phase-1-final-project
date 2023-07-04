0a7762f403207d4998f5886d8153ed1f427fdc39
Justin Rodriguez <justin.r@me.com>
2023-07-04 09:40:59 -0400

    add css for track list / play button

--- index.js
+++ index.js
@@ -11,18 +11,28 @@ function initialRender(){
             const imgContainer = document.createElement('div')
             const titleContainer = document.createElement('div')
             const img = document.createElement('img')
+            const playButton = document.createElement('button')
 
             trackContainer.className = 'track-container'
             imgContainer.className = 'img-container'
             titleContainer.className = 'title-container'
+            playButton.className = 'play-button'
+            playButton.id = data[x].id
             img.className = 'album-art'
             titleContainer.textContent = data[x].name['name-USen']
             img.src = data[x].image_uri
+            playButton.textContent = 'Play ▶️'
+
+            playButton.addEventListener('click', handlePlayButtonClick)
 
             imgContainer.appendChild(img)
-            trackContainer.append(imgContainer, titleContainer)
+            trackContainer.append(imgContainer, titleContainer, playButton)
             container.appendChild(trackContainer)
         }
     })
 
+}
+
+function handlePlayButtonClick(e){
+    console.log(e)
 }
