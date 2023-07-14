e7adb6fb5f3d73fb1261d6bb2594f171e13cbb52
Justin Rodriguez <justin.r@me.com>
2023-07-14 12:47:14 -0400

    refactor / optimization for index.js

--- index.js
+++ index.js
@@ -3,122 +3,89 @@ window.addEventListener('DOMContentLoaded', () => {
     document.getElementById('language').addEventListener('change', updateLanguage)
 })
 
-
-// The updatePage function accepts one parameter for language and is 
-// called in two instances during the execution of this program. 
-// 1. When the DOMContentLoaded event triggers
-// 2. When the user selects a different language from the dropdown
-// It fetches the data for all songs as an array then iterates over
-// that data to update the DOM with the tracklist 
 function updatePage(lang = 'name-USen'){
     fetch('http://acnhapi.com/v1a/songs')
     .then(res => res.json())
-    .then(data => {
-        const listContainer = document.getElementById('tracklist')
-        listContainer.innerHTML = '' // Important for avoiding duplicate data on the page
-
-        // Loads 'Welcome Horizons' as default song
-        updatePlayer(data[94], false, lang)
-
-        // Iterates over fetched song data to update DOM with tracklist
-        data.forEach(e => {
-            // Creates HTML elements to be appened to the DOM
-            const trackContainer = document.createElement('div')
-            const imgContainer = document.createElement('div')
-            const titleContainer = document.createElement('div')
-            const img = document.createElement('img')
-            const playButton = document.createElement('button')
-
-            // Adds data to the HTML elements from the data
-            trackContainer.className = 'track-container'
-            trackContainer.id = e.id
-            imgContainer.className = 'img-container'
-            titleContainer.className = 'title-container'
-            playButton.classList.add('play-button')
-            playButton.classList.add(lang) // Important for language persistence
-            playButton.id = e.id 
-            img.className = 'album-art'
-            titleContainer.textContent = `${e.id}. ${e.name[lang]}`
-            img.src = e.image_uri
-            playButton.textContent = 'Play ▶️'
-
-            playButton.addEventListener('click', handlePlayButtonClick)
-            
-            // Appends elements to the DOM
-            imgContainer.appendChild(img)
-            trackContainer.appendChild(imgContainer)
-            trackContainer.appendChild(titleContainer)
-            trackContainer.appendChild(playButton)
-            listContainer.appendChild(trackContainer)
-        });
+    .then(songData => {
+        updatePlayer(songData[94], false, lang)
+        updateTrackList(songData, lang)
     })
-
 }
 
-// The handlePlayButtonClick is an event handler for when the play button
-// on a track was clicked. The play button's id matches that of it's associate
-// song so the correct song's data can be retrieved. That data is then passed 
-// to the updatePlayer function along with a true value for the autoplay parameter
-// and the current selected language.
 function handlePlayButtonClick(e){
     fetch(`http://acnhapi.com/v1a/songs/${e.target.id}`)
     .then(res => res.json())
-    // Selected language was added to the classlist of the button to facilitate selected language persistence
     .then(data => updatePlayer(data, true, e.target.classList[1]))
 }
 
-// The updatePlayer function accepts three parameters: song, autoplay and lang.
-// It takes that data and updates the DOM with the album art, title of the song
-// an loads the src of the song into the auido element
 function updatePlayer(song, autoPlay = false, lang = 'name-USen'){
     const playerContainer = document.getElementById('player-div')
-    playerContainer.innerHTML = '' // Important for avoiding duplicate data on the page
+    playerContainer.innerHTML = ''
 
-    // Creates HTML elements to be appeneded to the DOM
     const fig = document.createElement('figure')
     const figcap = document.createElement('figcaption')
     const audio = document.createElement('audio')
     const albumArt = document.createElement('img')
 
-    // Adds data to the HTML elements from the data
     playerContainer.className = 'center'
     audio.autoplay = autoPlay
     audio.controls = true
-    audio.id = `${song.id}`
-    audio.className = lang
+    audio.id = song.id
     audio.src = song['music_uri']
     figcap.textContent = song.name[lang]
     albumArt.src = song['image_uri']
     albumArt.className = 'center'
-     
-    // Appends elements the DOM
-    fig.append(figcap, audio)
-    playerContainer.append(albumArt, fig)
 
     audio.addEventListener('ended', playNextTrack)
+     
+    fig.append(figcap, audio)
+    playerContainer.append(albumArt, fig)
 }
-
-// The updateLanguage function is an event handler for when the language
-// selection is changed in the dropdown menu. It takes that value and 
-// passes it to the updatePage function to update the tracklist accordingly.
 function updateLanguage(e){
     updatePage(e.target.value)
 }
 
-// The playNextTrack is an event handler for the 'ended' event added to the
-// audio HTML element. When the song ends, this function checks the track
-// number agains the the total tracks and plays the next song automatically. 
 function playNextTrack(e){
     let trackNumber = e.target.id
 
-    // Adds logic to autoplay the first track after the last has ended
     if (trackNumber < 95){
         trackNumber++
     }
     else trackNumber = 1
 
-    // Uses the result of that logic to update the player with the next track
     fetch(`http://acnhapi.com/v1a/songs/${trackNumber}`)
     .then(res => res.json())
     .then (data => updatePlayer(data, true, e.target.className))
+}
+
+function updateTrackList(songData, lang){
+
+    const listContainer = document.getElementById('tracklist')
+    listContainer.innerHTML = ''
+
+    songData.forEach(song => {
+        const trackContainer = document.createElement('div')
+        const imgContainer = document.createElement('div')
+        const titleContainer = document.createElement('div')
+        const img = document.createElement('img')
+        const playButton = document.createElement('button')
+
+        trackContainer.className = 'track-container'
+        trackContainer.id = song.id
+        imgContainer.className = 'img-container'
+        titleContainer.className = 'title-container'
+        playButton.classList.add('play-button')
+        playButton.classList.add(lang)
+        playButton.id = song.id 
+        img.className = 'album-art'
+        titleContainer.textContent = `${song.id}. ${song.name[lang]}`
+        img.src = song.image_uri
+        playButton.textContent = 'Play ▶️'
+
+        playButton.addEventListener('click', handlePlayButtonClick)
+        
+        imgContainer.appendChild(img)
+        trackContainer.append(imgContainer, titleContainer, playButton)
+        listContainer.appendChild(trackContainer)
+    });
 }
